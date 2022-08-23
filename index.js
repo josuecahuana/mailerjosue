5fb1bcb7e1bff7279d996904101bbdb4e08310c4
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-08-23 00:01:08 -0500

    cree un post

--- index.js
+++ index.js
@@ -12,17 +12,25 @@ let transporter = nodemailer.createTransport({
     }
 })
 
-let mailOptions = {
-    from: "josuemmca29@gmail.com",
-    to: "chispatriana29@gmail.com",
-    subject: "Prueba",
-    text: "Prueba"
-}
-
-transporter.sendMail(mailOptions, function(err, sucess){
-    if (err){
-        console.log(err)
-    } else{
-        console.log("Email sent")
+app.post('/sendEMail',function(req,res){
+    let mailOptions = {
+        from: "josuemmca29@gmail.com",
+        to: req.body.reciever,
+        subject: req.body.titulo,
+        text: req.body.text
     }
+    transporter.sendMail(mailOptions, function(err, sucess){
+        if (err){
+            console.log(err)
+        } else{
+            console.log("Email sent")
+        }
+    })
+
+
+
+})
+
+app.listen(36989, function(){
+    console.log("Started on PORT 3000")
 })
